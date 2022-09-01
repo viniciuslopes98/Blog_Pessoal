e34a035a1bdaab713181ebe5c218cd569cb44327
zm1hawk <vinicius6.lopes6@gmail.com>
2022-08-31 22:55:43 -0300

    aula 31-08-22

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,6 +3,7 @@ package com.generation.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
@@ -12,7 +13,7 @@ public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
 	//SELECT * FROM tb_postagens WHERE titulo LIKE "%%";
 	
-		public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+		public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
 	
 	//SELECT * FROM tb_postagens WHERE texto LIKE "%%";
 		public List <Postagem> findAllByTextoContainingIgnoreCase(String texto);
